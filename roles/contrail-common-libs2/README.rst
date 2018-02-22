ef7ceeaa05af023637abfb22304b80b527387499
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2018-02-22 17:02:20 +0100

    Import unittest related code (jobs, playbooks, roles)
    Move all jobs related to running unittests from contrail-project-config
    into contrail-zuul-jobs to make it possible to test them before merging.
    
    This is a part of a "review set":
    
      1. Enable shadowing of contrail-project-config in contrail-zuul-jobs
      2. Copy code (jobs, playbooks, etc.) over <-- here
      3. Remove job (and code) from contrail-project-config
    
    Change-Id: Id21eeaa0da84bdde24660c1cf6e4b4dc3ba9c006

---
+++ roles/contrail-common-libs2/README.rst
@@ -0,0 +1,4 @@
+Common set of libraries for OpencContrail andible tasks
+
+This role doesn't have any tasks defined, it's used as package
+for ansible modules that are used by other OpenContrail roles.
---
+++ roles/contrail-common-libs2/library/contrail_packaging.py
@@ -0,0 +1,97 @@
+import os
+import re
+
+from ansible.module_utils.basic import AnsibleModule
+from datetime import datetime
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+result = dict(
+    changed=False,
+    original_message='',
+    message='',
+)
+
+MASTER_RELEASE = '5.0.0'
+version_branch_regex = re.compile(r'^(master)|(R\d+\.\d+(\.\d+)?(\.x)?)$')
+
+
+class ReleaseType(object):
+    CONTINUOUS_INTEGRATION = 'continuous-integration'
+    NIGHTLY = 'nightly'
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            zuul=dict(type='dict', required=True),
+            release_type=dict(type='str', required=False, default=ReleaseType.CONTINUOUS_INTEGRATION),
+            build_number=dict(type='str', required=False, default='')
+        )
+    )
+
+    zuul = module.params['zuul']
+    release_type = module.params['release_type']
+    build_number = module.params['build_number']
+
+    branch = zuul['branch']
+    if not version_branch_regex.match(branch):
+        branch = 'master'
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    version = {'epoch': None}
+    if branch == 'master':
+        version['upstream'] = MASTER_RELEASE
+        version['public'] = 'master'
+        docker_version = 'master'
+    else:
+        version['upstream'] = branch[1:]
+        version['public'] = branch[1:]
+        docker_version = version['upstream']
+
+    if release_type == ReleaseType.CONTINUOUS_INTEGRATION:
+        # Versioning in CI consists of change id, pachset and date
+        change = zuul['change']
+        patchset = zuul['patchset']
+        version['distrib'] = "ci{change}.{patchset}".format(
+            change=change, patchset=patchset, date=date
+        )
+        repo_name = "{change}-{patchset}".format(change=change, patchset=patchset)
+    elif release_type == ReleaseType.NIGHTLY:
+        version['distrib'] = "{}".format(build_number)
+        docker_version = '{}-{}'.format(docker_version, build_number)
+        repo_name = docker_version
+    else:
+        module.fail_json(
+            msg="Unknown release_type: %s" % (release_type,), **result
+        )
+
+    debian_dir = None
+    for project in zuul['projects']:
+        if project['short_name'] == 'contrail-packages':
+            debian_dir = project['src_dir']
+    if debian_dir:
+        debian_dir = os.path.join(debian_dir, "debian/contrail/debian")
+    target_dir = "contrail-%s" % (version['upstream'],)
+
+    full_version = "{upstream}~{distrib}".format(**version)
+
+    packaging = {
+        'name': 'contrail',
+        'debian_dir': debian_dir,
+        'full_version': full_version,
+        'version': version,
+        'target_dir': target_dir,
+        'repo_name': repo_name,
+        'docker_version': docker_version,
+    }
+
+    module.exit_json(ansible_facts={'packaging': packaging}, **result)
+
+
+if __name__ == "__main__":
+    main()
