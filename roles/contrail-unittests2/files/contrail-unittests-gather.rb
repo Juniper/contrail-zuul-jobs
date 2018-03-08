fd1c403bdecb2219fb8a54067c92ef04b55072ef
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2018-03-08 20:23:17 +0100

    Add some debug output to contrail-unittest-gather.rb
    Change-Id: If8214c6774bfb1c79a596743fc042bb94f9dfe22

--- roles/contrail-unittests2/files/contrail-unittests-gather.rb
+++ roles/contrail-unittests2/files/contrail-unittests-gather.rb
@@ -6,6 +6,7 @@ require 'json'
 exit(0) if ENV["ZUUL_CHANGES"] !~ /refs\/changes\/([^^]*)$/
 change_set = $1
 
+STDERR.puts "contrail-unittest-gather.rb: Choosing tests to execute\n"
 contrail_sources = "#{ENV["WORKSPACE"]}/contrail-#{ENV["UPSTREAM_VERSION"]}"
 
 json_file = "#{contrail_sources}/controller/ci_unittests.json"
@@ -18,15 +19,22 @@ project = "vrouter" if ENV["ZUUL_PROJECT"] =~ /contrail-vrouter/
 project = "src/contrail-common" if ENV["ZUUL_PROJECT"] =~ /contrail-common/
 project = "src/contrail-analytics" if ENV["ZUUL_PROJECT"] =~ /contrail-analytics/
 
+STDERR.puts "contrail-unittest-gather.rb: Review for project #{ENV["ZUUL_PROJECT"]}\n"
+STDERR.puts "contrail-unittest-gather.rb: Check for commits for #{project}\n"
+
 Dir.chdir("#{contrail_sources}/#{project}")
 
 # Get the files changes in this change-set.
-cmd = %{git ls-remote gerrit 2>/dev/null | \grep #{change_set} | \grep refs | awk '{print $1}' | xargs git show --pretty="format:" --name-only}
+cmd = %{git ls-remote origin 2>/dev/null | \grep #{change_set} | \grep refs | awk '{print $1}' | xargs git show --pretty="format:" --name-only}
 
 @dirs = { }
 `#{cmd}`.split.each { |file|
     next if "#{project}/#{file}" !~ /(.*?\/.*?\/.*?)\//
     @dirs[$1] = true
+}
+STDERR.puts "contrail-unittest-gather.rb: List of directories changed:\n"
+@dirs.each_key { |dir|
+    STDERR.puts "contrail-unittest-gather.rb:\t#{dir}\n"
 }
 
 # Always test for changes to generateds and vrouter projects.
@@ -42,17 +50,18 @@ json.each_pair { |module_name, module_data|
     skip = true
     @dirs.each_key { |dir|
         if module_data["source_directories"].include?(dir) then
-
             # We need to run associated tests as the path matches.
             skip = false
             break
         end
     }
     next if skip
+    STDERR.puts "contrail-unittest-gather.rb: SCons targets to run: #{module_data['scons_test_targets']}\n"
 
     @tests += module_data["scons_test_targets"]
     module_data["misc_test_targets"].each { |m|
         @tests += json[m]["scons_test_targets"]
+	STDERR.puts "contrail-unittest-gather.rb: SCons misc targets: #{json[m]['scons_test_targets']}\n"
     }
 }
 
