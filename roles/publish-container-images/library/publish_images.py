1029f43bef0ecc45300ae4091e6ec9f131f0a65a
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2018-04-03 13:14:28 +0200

    Create a role for uploading container images
    Change-Id: I20c7812294423a7b2137ba5b61be9129273776ed

---
+++ roles/publish-container-images/library/publish_images.py
@@ -0,0 +1,72 @@
+import os
+import re
+
+from ansible.module_utils.basic import AnsibleModule
+from datetime import datetime
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+result = dict(
+    changed=False,
+    message='',
+)
+
+def get_image(client, full_name):
+    images = client.images()
+    for i in images:
+        if full_name in i['RepoTags']:
+            return i
+    return None
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            images=dict(type='list', required=True),
+            tag=dict(type='str', required=True),
+            registry=dict(type='str', required=True),
+        )
+    )
+    params = module.params
+
+    # We import docker here, to provide a useful message back
+    # to ansible in case the import fails.
+    try:
+        import docker
+    except ImportError as e:
+        module.fail_json(msg="Couldn't import docker package: %s" % (str(e),), **result)
+
+    client = docker.from_env()
+
+    images = []
+    for i in params['images']:
+        image_name = ":".join((i['name'], i['tag']))
+        repository = "/".join((params['registry'], i['name']))
+        try:
+            client.tag(image_name, repository=repository, tag=params['tag'])
+        except docker.errors.NotFound:
+            module.fail_json(
+                msg="Missing local image: %s" % (image_name,), **result)
+        except Exception as e:
+            module.fail_json(
+                msg="Unknown error: %s" % (str(e),))
+        images += [repository]
+
+    for image_repository in images:
+        try:
+            client.push(image_repository, tag=params['tag'],
+                        insecure_registry=True)
+        except Exception as e:
+            msg = "Unknown error while pushing %s: %s" % (
+                ":".join((image_repository, params['tag'])), str(e),
+            )
+            module.fail_json(msg=msg, **result)
+
+    module.exit_json(**result)
+
+
+if __name__ == "__main__":
+    main()
