5ac6a26bd7cb9a9dc6df347c31b1f875379c51e6
Michal Krawczyk <michal.krawczyk@codilime.com>
2018-07-03 15:55:22 +0200

    Enforce ANSIBLE-J0002 ansible style guide rule
    Change-Id: Ifd24bfaaae239c76551be2fcb2052fc1dd2b299d

---
+++ ansible-lint-rules/YAMLFileExtension.py
@@ -0,0 +1,19 @@
+from ansiblelint import AnsibleLintRule
+
+
+class YAMLFileExtension(AnsibleLintRule):
+    id = 'ANSIBLE-J0002'
+    shortdesc = 'YAML files should have the .yaml file extension.'
+    description = ''
+    tags = ['formatting']
+    done = []  # already noticed path list
+
+    def match(self, file, text):
+        if file['type'] != 'playbook':
+            return False
+
+        path = file['path']
+        if not path.endswith('.yaml') and path not in self.done:
+            self.done.append(path)
+            return True
+        return False
