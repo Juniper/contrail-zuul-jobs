92341a132ddb7958a4677d562c3d8bcfbfec2834
Przemysław Marcinkiewicz <przemyslaw.marcinkiewicz@codilime.com>
2018-10-19 12:46:30 +0200

    Add url to jenkins job in zuul tentacle output.
    Change-Id: Ia824377201f51a9a82ab1408f11c827ec1e3406d

--- roles/build-windows-containers/files/zuulstentacle.py
+++ roles/build-windows-containers/files/zuulstentacle.py
@@ -11,6 +11,7 @@ import threading
 import urllib.error
 import urllib.request
 import urllib.response
+import re
 
 tool_description =\
         "Zuul's Tentacle - simple tool for integrating Zuul v3 with Jenkins"
@@ -118,7 +119,12 @@ class JenkinsManager:
         qi = job.invoke(build_params=job_parameters, block=True)
         print("QI: {}" .format(qi))
         build = qi.get_build()
+        last_build = job.get_last_buildnumber()
         job_status = build.get_status()
+        print("last_bulld",last_build)
+        full_job_name = job.get_full_name()
+        names = re.split('/', full_job_name)
+        print("Job log {}/job/{}/job/{}/{}".format(self.jenkins_address, names[0], names[1], last_build))
         print("Jenkins job finished with status: {}".format(job_status))
         job_was_successful = (job_status == jenkinsapi.constants.STATUS_SUCCESS)
         return job_was_successful
