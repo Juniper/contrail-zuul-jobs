f964c1b682ac68a8a1bcb15300fb03bcfa9e7065
Michal Krawczyk <michal.krawczyk@codilime.com>
2018-11-26 09:41:43 +0100

    DNM fix target for running unit tests
    Change-Id: Ib8697f84c3d1c076ba97a42db294c5f34f141c87

--- roles/contrail-unittests2/files/contrail-unittests-job.sh
+++ roles/contrail-unittests2/files/contrail-unittests-job.sh
@@ -212,7 +212,7 @@ function run_unittest() {
 
     logfile=$WORKSPACE/scons_test.log
     echo scons --debug=explain -k -j $SCONS_JOBS $UNIT_TESTS
-    scons -k --debug=explain -j $SCONS_JOBS $UNIT_TESTS | tee $logfile
+    scons -k --debug=explain -j $SCONS_JOBS 'controller/src/xmpp:test' | tee $logfile
     exit_status=$?
     analyze_test_results $logfile
     # If unit test pass, show the results and exit    
