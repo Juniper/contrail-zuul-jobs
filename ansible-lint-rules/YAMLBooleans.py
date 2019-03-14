a82de63bb45f4b4f80784e750c4a35e554892dd2
Michał Czuba <michal.czuba@codilime.com>
2019-03-14 11:19:27 +0100

    Add YAMLBooleans.py to ansible-lint-rules
    ANSIBLE-J0007:
    Check if booleanes was declared with true/false (lower score).
    
    Corrected Ansible playbooks according to this rule.
    Added test playbooks for this rule.
    Removed some quoting from task names (ANSIBLE-J0006).
    
    Depends-On: I3a0d8715059d6e4bc6d5c96c0f7a2c719a1cc777
    Depends-On: I09e581a4498210d0f515e4b4de96af10e2b0da7f
    
    Partial-Jira-bug: JD-594
    
    Change-Id: I7027b2d0776110113a1cdaed5b9f6dc0b1692dfd

---
+++ ansible-lint-rules/YAMLBooleans.py
@@ -0,0 +1,13 @@
+from ansiblelint import AnsibleLintRule
+import re
+
+class YAMLBooleans(AnsibleLintRule):
+    id = 'ANSIBLE-J0007'
+    shortdesc = 'Booleans should be defined with true/false (lower case) values.'
+    description = 'Booleans should be defined with true/false (lower case) values.'
+    tags = ['formatting']
+
+    literal_bool = re.compile('[a-z]: ?([Yy]es$|[Nn]o$|True$|False$)')
+
+    def match(self, file, line):
+        return self.literal_bool.search(line)
