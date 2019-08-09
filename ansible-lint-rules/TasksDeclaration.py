987988833903dd2c225bccee73347c8f52aba2e4
Andrii Moshurenko <andrii.moshurenko@codilime.com>
2019-08-09 15:28:57 +0200

    fix task declaration linter rule ANSIBLE-J0004
    Change-Id: I8eefb946f4050210327b1f883b50714e7f7ba18d

--- ansible-lint-rules/TasksDeclaration.py
+++ ansible-lint-rules/TasksDeclaration.py
@@ -1,26 +1,20 @@
-from ansiblelint import AnsibleLintRule
-
-
-class TasksDeclaration(AnsibleLintRule):
+from ansiblelint import AnsibleLintRule class TasksDeclaration(AnsibleLintRule):
     id = 'ANSIBLE-J0004'
     shortdesc = 'Tasks declaration should have a specific form.'
     description = 'N/A'
     tags = ['formatting']
-
     def check_tasks_name(self, file, play):
         declaration_errors = []
-        for task in play['tasks']:
-            task_name = task['name']
-            if not task_name[0].islower():
-                declaration_errors.append(({task_name: '...'},
-                                           'Line: {} - Task name: [{}] - should start with lowercase!'.format(
-                                               task['__line__'], task_name)))
+        task_name = play['name']
+        if not task_name[0].islower():
+            declaration_errors.append(({task_name: '...'},
+                'Line: {} - Task name: [{}] - should start with lowercase!'.format(
+                play['__line__'], task_name)))
         return declaration_errors
-
     def matchplay(self, file, play):
         result = []
         if file['type'] != 'playbook':
             return False
-        if 'tasks' in play:
+        if 'name' in play:
             result += self.check_tasks_name(file, play)
-        return result
+            return result
