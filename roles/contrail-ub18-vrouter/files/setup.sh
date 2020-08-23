251c038bf8c99b134e7e1eacd96a033d51a13b8b
Vinay Mahuli <vmahuli@juniper.net>
2020-08-24 02:23:22 +0530

    Integrate ub18 vrouter build job with tf-vrouter
    Closes-Jira-Bug: CE-7367
    
    Change-Id: I67c6a91cf2c43084b55c0928dda9b257f60b4271
    Depends-On: Ifbb996df0841910419a3f6d2687446a121b60222

---
+++ roles/contrail-ub18-vrouter/files/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -xe
+
+# update your existing list of packages
+sudo apt update
+
+# Next, install a few prerequisite packages which let apt use packages over HTTPS
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
+# Then add the GPG key for the official Docker repository to your system
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add the Docker repository to APT sources
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Next, update the package database with the Docker packages from the newly added repo
+sudo apt-get -y update
+
+# Make sure you are about to install from the Docker repo instead of the default Ubuntu repo
+sudo apt-cache policy docker-ce
+
+# Finally, install Docker
+sudo apt-get install -y docker-ce
+
+Docker should now be installed, the daemon started
+sudo systemctl status docker
+
+# If you want to avoid typing sudo whenever you run the docker command, add your username to the docker group
+#sudo usermod -aG docker ${USER}
